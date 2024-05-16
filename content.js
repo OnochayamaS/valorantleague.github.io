32ba72d48bb0656249ab34e772cde4d4779d3de4
OnochayamaS <winkug1@gmail.com>
2024-05-16 21:48:44 +0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,11 @@
+
+function openNav(){
+    document.getElementById("mySidenav").style.width = "452px";
+    document.getElementById("bar").style.marginLeft = "452px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("bar").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
